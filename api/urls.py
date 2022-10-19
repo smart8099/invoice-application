d930a6d1236b101b04048181461f6ba410973bad
smart8099 <abdulbasitm810@gmail.com>
2022-10-18 22:17:26 -0700

    Create Update Invoices endpoint

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.getInvoices),
-    path('add/',views.addInvoice),
-    path('invoice/<int:pk>/',views.get_invoice_detail)
-
+    path('invoices',views.getInvoices),
+    path('invoices/add/',views.addInvoice),
+    path('invoices/<int:pk>/',views.get_invoice_detail),
+    path('invoices/update/<int:pk>',views.update_invoice)
 ]
+
